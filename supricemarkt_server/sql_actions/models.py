e56f9c5237c0db801af48f9a0861d18f142d16dd
Gbatz <carloszaragozabeato15@gmail.com>
2023-03-23 15:06:35 +0100

    s

--- supricemarkt_server/sql_actions/models.py
+++ supricemarkt_server/sql_actions/models.py
@@ -10,12 +10,14 @@ class Supermercado(models.Model):
 
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100, blank=False, null=False)
+    id = models.AutoField(auto_created=True ,primary_key=True)
+    nombre = models.CharField(max_length=200, blank=False, null=False)
     precio = models.FloatField(blank=False, null=True)
-    imagen = models.CharField(max_length=255, null=True)
+    imagen = models.CharField(max_length=300, null=True)
     numero_veces = models.IntegerField(blank=False, null=False, default=0)
     supermercado = models.ForeignKey(Supermercado, on_delete=models.CASCADE)
     
+
     
     def __str__(self):
-        return self.title
+        return f"{self.nombre} {self.precio} {self.imagen} {self.numero_veces} {self.supermercado.nombre}"
--- supricemarkt_server/sql_actions/views.py
+++ supricemarkt_server/sql_actions/views.py
@@ -7,20 +7,54 @@ from django.http import JsonResponse
 from . import models
 from django.views.decorators.csrf import ensure_csrf_cookie
 
-@ensure_csrf_cookie
+
 def InsertProduct(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         nombre = data['nombre']
-        precio = data['precio']
+        precio = data['precio'].replace("€","")
+        precio = float(precio.replace(",","."))
         image = data['image']
         supermercado_nombre = data['supermercado']
         
         
-        all_supermarkets = models.Supermercado.objects.all()
+        all_supermarkets = list(models.Supermercado.objects.all())
+        all_products = list(models.Producto.objects.all())
         
-        print(all_supermarkets)
+        numero_veces = 1
         
+        for supermarket in all_supermarkets:
+            if supermercado_nombre == supermarket.nombre:
+                supermercado = supermarket
         
-        # hacer algo con los datos recibidos
+    
+    
+        update:bool = False
+
+
+        for producto in all_products:
+            if producto.nombre == nombre and supermercado.nombre == producto.supermercado.nombre:
+                numero_veces = producto.numero_veces+1
+
+                producto2 = models.Producto(
+                                        supermercado=supermercado, 
+                                        imagen=image,
+                                        nombre=nombre,
+                                        numero_veces=numero_veces,
+                                        precio=precio)
+                producto.delete()
+                producto2.save()
+                update = True
+                
+        else:
+            if not update:
+                producto = models.Producto(supermercado=supermercado, 
+                                            imagen=image,
+                                            nombre=nombre,
+                                            numero_veces=numero_veces,
+                                            precio=precio)
+                producto.save()
+
+
+
         return JsonResponse({'status': 'ok'})
